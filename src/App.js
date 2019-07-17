4ce9afff8dc02bd9e5d03b9914b01230d21bc1f8
Clayton <45694223+Clayton-GitHub@users.noreply.github.com>
2019-07-16 22:04:47 -0400

    I

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
-import Cards from './Cards'
+import Cards from './Cards';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
 
   render() {
-    return <div>
-        <div>
-          
+    return <div className='container-fluid'>
+      <div className='row'>
+        <div className='col-sm-3 px-1 bg-dark'>
+          <div className='py-2 sticky-top flex-grow-1'>
+            <div className='nav flex-sm-column'>
+              <span>Sidebar Header</span>
+              <span>Sidebar Header</span>
+              <span>Sidebar Header</span>
+              <span>Sidebar Header</span>
+            </div>          
+          </div>
         </div>
-        <div>
+        <div className='col'>
           <Cards />
         </div>
-      </div>
+      </div>        
+    </div>
       
   }
   
